fdbd180ee1a3b48fb15c748565e1d30cd474ff52
uri <13288uri@gmail.com>
2025-06-17 16:41:02 +0300

    cr fixes

--- fold/Fold.cpp
+++ fold/Fold.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-#define MAX_TABLE_NUMBER (11)
-#define TABLE_ITEM_LENGTH (6)
+const unsigned int MAX_TABLE_NUMBER = 11;
+const unsigned int TABLE_ITEM_LENGTH = 6;
 
 using std::cout;
 using std::endl;
 
 /**
- * Get the number of digits of a positive number
- * 
+ * @brief Get the number of digits of a positive number
+ *
  * @param num       [IN]    The number
  * @return the number of digits of num
  */
-int getNumOfDigits(int num) {
-    int digits = 0;
+unsigned int getNumOfDigits(unsigned int num) {
+    unsigned int digits = 0;
     while (num > 0) {
         num /= 10;
         digits++;
@@ -22,33 +22,32 @@ int getNumOfDigits(int num) {
 }
 
 /**
- * Print a number of whitespaces
- * 
+ * @brief Print a number of whitespaces
+ *
  * @param spaces    [IN]    Number of whitespaces to print
  */
-void printSpaces(int spaces) {
-    int i = 0;
+void printSpaces(unsigned int spaces) {
+    unsigned int i = 0;
     for (i = 0; i < spaces; i++) {
         cout << " ";
     }
 }
 
-int main()
-{
-    int i = 1;
-    int j = 1;
-    int current_number = 0;
-    int current_digits = 0;
-    
-    for (i = 1; i < MAX_TABLE_NUMBER + 1; i++) {
+int main() {
+    unsigned int i = 1;
+    unsigned int j = 1;
+    unsigned int currentNumber = 0;
+    unsigned int currentDigits = 0;
+
+    for (i = 1; i <= MAX_TABLE_NUMBER; i++) {
         for (j = 1; j < MAX_TABLE_NUMBER; j++) {
-            current_number = i * j;
-            current_digits = getNumOfDigits(current_number);
-            cout << current_number;
-            printSpaces(TABLE_ITEM_LENGTH - current_digits);
+            currentNumber = i * j;
+            currentDigits = getNumOfDigits(currentNumber);
+            cout << currentNumber;
+            printSpaces(TABLE_ITEM_LENGTH - currentDigits);
         }
-        current_number = i * MAX_TABLE_NUMBER;
-        cout << current_number << endl;
+        currentNumber = i * MAX_TABLE_NUMBER;
+        cout << currentNumber << endl;
     }
 
     return 0;
